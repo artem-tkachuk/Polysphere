467a550e0679ba3716f2a4a667d9462a247356f2
zee6197 <zeeman@berkeley.edu>
2023-06-18 10:59:28 -0700

    Updated the SQL query into a python wrapper function

--- spotify_code/mindsDB.py
+++ spotify_code/mindsDB.py
@@ -3,7 +3,15 @@ import mindsdb_sdk
 # Connect to cloud server
 server = mindsdb_sdk.connect('https://cloud.mindsdb.com', login='zeeman6197@gmail.com', password='art101')
 
-project = server.get_project()
 
-query = project.query("SELECT * FROM mindsdb.dalle2 WHERE text = 'a happy face'");
-print(query.fetch().loc[0, 'img_url'])
+def textToImage(description = 'a happy face'): 
+
+    project = server.get_project()
+
+    query = project.query(f"SELECT * FROM mindsdb.dalle2 WHERE text = '{description}' ");
+    print(query.fetch().loc[0, 'img_url'])
+
+textToImage()
+
+
+
